2e88cf42bb54335550142a13136448471f10f21d
LIGHThecoder <162660405+Lthecoders@users.noreply.github.com>
2024-09-16 11:11:03 +0530

    done till practical 6

---
+++ 06 practical/01_code.py
@@ -0,0 +1,35 @@
+
+# print("Nikhil")
+
+# *Write Python program to perform following
+# operations on List:
+# a) Create
+# b) Access
+# c) Update
+# d) Delete elements from list.
+
+
+# creating list
+my_list = ['nikhil','rahul','rajendra']
+# print(my_list)
+
+# access list
+# for access_items in my_list:
+#     print(access_items) 
+
+
+
+# update list
+my_list = ['nikhil','rahul','rajendra'] # original list
+my_list.append('Honey') # adding items in list
+my_list.append('Light') # adding items in list
+print(my_list)
+
+
+# delet elements from list
+my_list = ['nikhil', 'rahul', 'rajendra', 'Honey', 'Light']
+my_list.remove('Light')
+my_list.remove('Honey')
+print(my_list)
+
+
